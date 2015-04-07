4897fe6c63f5e1355765bd6aad78ab0c360ca897
hkang408 <hkang408@gmail.com>
2015-04-07 14:20:23 -0700

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -9,20 +9,24 @@ def word_count(gene, h):
             h[word] = new_val
     return h
 
-def phage_map(path,name):    #h = hash of gene names
+def phage_map(path, genome):    #h = hash of gene names
     counter = 0
     switch = 0
     pp_counter = 0
     try:
         a = open(str(path)+'/prophage_tbl.txt', 'rU')
     except:
- #       print str(path) + ':file not found'
+        print str(path) + ':file not found'
         return
 
     irofile = iter(a)
     HASH = {}
-    for line in irofile:
+    a.readline()
+    for line in a:
         i = line.split('\t')
+        name = str(i[1]).rstrip()
+        name = name.upper()
+
         if switch == 0: # not in pp
             if i[9].startswith('0'): ## Is not a phage
                 pass
@@ -30,16 +34,17 @@ def phage_map(path,name):    #h = hash of gene names
                 MAP  = []
                 switch = 1
                 pp_counter +=1
-                contig = i[2]
-                MAP.append(str(i[1]))
+                contig = i[2].rstrip()
+                MAP.append(ID(name))
         
         elif switch == 1:
             if (i[9].startswith('1') and contig == i[2]): #Same prophage continued.
                 pp_counter += 1
-                MAP.append(str(i[1]))
-                contig = i[2]
+                MAP.append(ID(name))
+                
             elif (i[9].startswith('1') and contig != i[2]):   # contigs changed in pp. .: 2 prophages
                 counter += 1
+                contig = i[2]                          
                 if int(pp_counter) < 3:
                     MAP = []
                     counter -= 1
@@ -50,8 +55,9 @@ def phage_map(path,name):    #h = hash of gene names
                     counter -= 1
                     pp_counter = 1
                 else:
-                    HASH[str(name)+'_'+str(counter)] = MAP
+                    HASH[str(genome)+'_'+str(counter)] = MAP
                     MAP = []
+                    MAP.append(ID(name))
                     pp_counter = 1
             elif i[9].startswith('0'):
                 counter += 1
@@ -65,67 +71,65 @@ def phage_map(path,name):    #h = hash of gene names
                     counter -= 1
                     pp_counter = 1
                 else:
-                    HASH[str(name)+'_'+str(counter)] = MAP
+                    HASH[str(genome)+'_'+str(counter)] = MAP
                     MAP = []
-                    pp_counter = 1
+                    pp_counter = 0
     a.close()
     return HASH
 
 def automate():
-    a = open('whole_genomes.txt','U')
-    b = open('whole_genome_pp.txt', 'w')
+    a = open('genomesNR.txt','U')
+    b = open('genome_heatmapNR.txt', 'w')
 
     for i in a:
         i = i.strip()
         i = i.replace('\n','')
         path = '/home3/katelyn/KBase/prophages/SEED/'+str(i)
         
- #       try:
-        HASH = phage_map(path,i)
-        for pp in HASH:
-            LIST = HASH[pp]     # LIST contains array of each gene name encountered in prophage region
-            ORDER = genemap(LIST)
-            b.write(str(pp)+'\t')
-            for j in ORDER:
-                b.write(str(j)+'\t')
-            b.write('\n')
+        try:
+            HASH = phage_map(path,i)
+            for pp in HASH:
+                LIST = HASH[pp]     # LIST contains array of each gene name encountered in prophage region
+                b.write(str(pp))
+                for each in LIST:
+                    b.write('\t' + str(each))
+                b.write('\n')
                     
-#        except:
- #           pass
+        except:
+            pass
     a.close()
     b.close()
 
-def genemap(LIST):
-    gene_dictionary = {
-    'INTEGRASE':1,
-    'PORTAL':2,
-    'TAIL': 3,
-    'HOLIN':4,
-    'TRANSPOSA':5,
-    'TERMINA':6,
-    'ENDONUCLEAS':7,
-    'CAPSID':8,
-    'HOLLIDAY':9,
-    'BASEPLATE':10,
-    'PROTEASE':11,
-    'LYSIN':12,
-    'TOXI':13,
-    'LYSOZYM':14}
-    SEQ = []
-    for gene in LIST:
-        flag = 0
-        counter = 1
-        for key in gene_dictionary.keys():
-            if flag == 1: # Key had hit
-                pass
-            elif flag == 0:
-                if key in gene.upper():
-                    SEQ.append(gene_dictionary[key])
-                    flag = 1
-                else:
-                    counter +=1
-            if counter == 15:
-                SEQ.append(0)
-                counter +=1
-    return SEQ
+def ID(name):
+    if 'INTEGRASE' in name:
+        return 'A'
+    elif 'PORTAL' in name:
+        return 'B'
+    elif 'TAIL' in name:
+        return 'C'
+    elif 'HOLIN' in name:
+        return 'D'
+    elif 'CAPSID' in name:
+        return 'E'
+    elif 'TERMINASE' in name:
+        return 'F'
+    elif 'ENDONUCLEAS' in name:
+        return 'G'
+    elif 'TRANSPOSAS' in name:
+        return 'H'
+    elif 'HOLLIDAY' in name:
+        return 'I'
+    elif 'BASEPLATE' in name:
+        return 'J'
+    elif 'PROTEASE' in name:
+        return 'K'
+    elif 'LYSIN' in name:
+        return 'L'
+    elif 'TOXIC' in name:
+        return 'M'
+    elif 'LYSOZYM' in name:
+        return 'N'
+    else:
+        return 'O'
 
+automate()
