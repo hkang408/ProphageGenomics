04c96f5b3165fb41b249b19b9876ed4818b3ada9
hkang408 <hkang408@gmail.com>
2015-06-12 12:10:51 -0700

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -68,7 +68,7 @@ def phage_map(path, genome):    #h = hash of gene names
 
 def automate():
     a = open('genomesNR.txt','U')
-    b = open('genome_heatmapNR.txt', 'w')
+    b = open('genomeNR_heatmap.txt', 'w')
 
     for i in a:
         i = i.strip()
@@ -100,53 +100,47 @@ def ID(name):
         return 'D'
     elif 'CAPSID' in name:
         return 'E'
-    elif 'TERMINASE' in name:
-        return 'F'
     elif 'ENDONUCLEAS' in name:
-        return 'G'
+        return 'F'
     elif 'TRANSPOSAS' in name:
-        return 'H'
-    elif 'HOLLIDAY' in name:
-        return 'I'
+        return 'G'
     elif 'BASEPLATE' in name:
-        return 'J'
+        return 'H'
     elif 'PROTEASE' in name:
-        return 'K'
+        return 'I'
     elif 'LYSIN' in name:
+        return 'J'
+    elif 'TERMINASE' in name:
+        return 'K'
+    elif 'DNA POLYMERASE' in name:
         return 'L'
-    elif 'TOXIC' in name:
+    elif 'SINGLE STRANDED DNA BINDING' in name.replace('-',' '):
         return 'M'
-    elif 'LYSOZYM' in name:
+    elif 'HYPOTHETICAL' in name:
         return 'N'
     else:
         return 'O'
-
-
 '''
 '''
 def pick(array):
-    GENELIST = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O']
-    for each in array:
-        if each in GENELIST:
-            GENELIST.remove(each)
-    
-    if 'A' not in GENELIST:
-        if len(GENELIST) < 10:
-            return 1
-        else:
-            return 0
+    if 'A' in array:
+        if 'B' in array:
+            if 'C' in array:
+                if 'D' in array:
+                    if 'E' in array:
+                        return True
     else:
-        return 0
+        return False
 
 def filter_heatmap():
-    a = open('genome_heatmapNR.txt', 'rU')
-    b = open('NR5_heatmap.txt', 'w')
+    a = open('genomeNR_heatmap.txt', 'rU')
+    b = open('5_heatmapNR.txt', 'w')
     for line in a:
         line = line.rstrip()
         i = line.split('\t')
         genes = i[1:]
         check = pick(genes)
-        if check == 1:
+        if check == True:
             int_ = (genes.index('A')+1.0)/len(genes)
             if int_ < 0.5:
                 b.write(line+'\n')
@@ -155,7 +149,30 @@ def filter_heatmap():
                 for each in reversed(genes):
                     b.write('\t'+str(each))
                 b.write('\n')
-        elif check == 0:
+        elif check == False:
             pass
-        
 
+def reorder():
+    a = open('5_heatmapNR.txt', 'rU')
+    b = open('INTfront_heatmapNR.txt', 'w')
+    for line in a:
+        i = line.split('\t')
+        genes = i[1:]
+        if genes.index('A') == 0:
+            b.write(line)
+        elif genes.index('A') != 0:
+            int_pos = genes.index('A')
+            left = genes[0:int_pos]
+            right = genes[int_pos:]
+            new_genes = right + left
+            b.write(i[0])
+            for each in new_genes:
+                each = each.rstrip()
+                b.write('\t'+each)
+            b.write('\n')
+    a.close()
+    b.close()
+
+automate()
+filter_heatmap()
+reorder()
