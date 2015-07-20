9e267d3425c5931737ce6d24ee7f1046fa65150f
hkang408 <hkang408@gmail.com>
2015-07-20 13:39:39 -0700

    Update curate_prophages.py

--- curate_prophages.py
+++ curate_prophages.py
@@ -11,11 +11,7 @@ def pp_get(path,name):
     switch = 0 # pp designator
     int_pos = 0
     hypo_counter = 0
-    t_id = ''
-    t_name = ''         # t for temp, l for left, r for right
 
-    l_id = ''
-    l_name = ''
     b = open('pp_summary.txt', 'a+')
     try:
         a = open(path+'/prophage_tbl.txt', 'rU')
@@ -26,9 +22,6 @@ def pp_get(path,name):
     irofile = iter(a)
     int_id = 0
     int_pos = 0
-    repeat_l = 0
-    repeat_r = 0
-    repeat_reason = 0
     
     for line in irofile:
         i = line.split('\t')
@@ -39,7 +32,7 @@ def pp_get(path,name):
             elif i[9].startswith('1'): ## Beginning of phage
                 switch = 1
                 pp_counter += 1
-                if 'hypothetical' in i[1] or 'putative' in i[1] or 'Hypothetical' in i[1] or 'Putative' in i[1]:
+                if 'HYPOTHETICAL' in i[1].upper() or 'PUTATIVE' in i[1].upper():
                     hypo_counter += 1
                 if int(i[3]) < int(i[4]):
                     start = i[3]
@@ -49,16 +42,7 @@ def pp_get(path,name):
                      print 'error in setting start position'
                          
                 contig = i[2]
-                try:                    # This only works when the repeat designation is at the start of pp region...
-                    repeat_l = i[14]
-                    repeat_r = i[15]
-                    repeat_reason = i[16]
-                    repeat_reason = repeat_reason.rstrip()
-                except:
-                    repeat_l = 0
-                    repeat_r = 0
-                    repeat_reason = 0
-                if ('integrase' in i[1] or 'Integrase' in i[1]):        # Not all will have an integrase... and some may have multiple.
+                if 'INTEGRASE' in i[1].upper():        # Not all will have an integrase... and some may have multiple.
                     int_pos = pp_counter
                     int_id = i[0]
 
@@ -66,7 +50,7 @@ def pp_get(path,name):
         elif switch == 1:
             if (i[9].startswith('1') and contig == i[2]): #Same prophage continued.
                 pp_counter +=1
-                if 'hypothetical' in i[1] or 'putative' in i[1] or 'Hypothetical' in i[1] or 'Putative' in i[1]:
+                if 'HYPOTHETICAL' in i[1].upper() or 'PUTATIVE' in i[1].upper():
                     hypo_counter += 1
                 if int(i[3]) > int(i[4]):
                     end = i[3]
@@ -74,17 +58,14 @@ def pp_get(path,name):
                     end = i[4]
                 else:
                      print 'error in setting end position'
-
                 
-                if ('integrase' in i[1] or 'Integrase' in i[1]):
+                if 'INTEGRASE' in i[1].upper():        # Not all will have an integrase... and some may have multiple.
                     int_pos = pp_counter                # This would overwrite any pp found at beginning of integrase
                     int_id = i[0]               
 
             elif (i[9].startswith('1') and contig != i[2]):   # contigs changed in pp. .: 2 prophages
                 count +=1
                 try:
-                    r_id = 0
-                    r_name = 0
                     pp_len = int(end) - int(start) # Uses previous end value
                     total_genes = pp_counter
                     integrase_position = (int(int_pos)*1.0)/int(pp_counter)*1.0
@@ -96,9 +77,6 @@ def pp_get(path,name):
                     print str(i[0]) + ': Region too small.'
                     int_id = 0
                     int_pos = 0
-                    repeat_l = 0
-                    repeat_r = 0
-                    repeat_reason = 0
                     pp_counter = 0
                     start = 0
                     hypo_counter = 0
@@ -106,25 +84,16 @@ def pp_get(path,name):
                 elif int(pp_counter) >= 5:
                     b.write(
                     name+'*' + '\t' + str(count) + '\t' + str(contig) + '\t' + str(start) + '\t' + str(end) + '\t'
-                    + str(l_id) + '\t' + str(l_name) + '\t' + str(r_id) + '\t' +str(r_name) + '\t' + str(hypo_perc) + 
+                    +  str(hypo_perc) + 
                     '\t')
-
                     if int_id != 0:                                     # If integrase has been found
                         b.write(str(int_id) + '\t' + str(integrase_position) + '\t')
-                    else:
-                        b.write('\t\t')
-
-                    if repeat_l !=0:                                    # If successful in finding repeat region
-                        b.write(str(repeat_l) + '\t' + str(repeat_r) + '\t' + str(repeat_reason))
                     else:
                         b.write('\t\t')
                     b.write('\n')
                 
                 int_id = 0
                 int_pos = 0
-                repeat_l = 0
-                repeat_r = 0
-                repeat_reason = 0
                 pp_counter = 1
                 start = 0
                 hypo_counter = 0
@@ -139,8 +108,6 @@ def pp_get(path,name):
             elif i[9].startswith('0'): # Reached the end of pp
                 count+=1
                 try:
-                    r_id = i[0]
-                    r_name = i[1]
                     pp_len = int(end) - int(start)
                     total_genes = pp_counter
                     integrase_position = (int(int_pos)*1.0/int(pp_counter))*1.0
@@ -151,9 +118,6 @@ def pp_get(path,name):
                 if int(pp_counter) < 5:
                     int_id = 0
                     int_pos = 0
-                    repeat_l = 0
-                    repeat_r = 0
-                    repeat_reason = 0
                     pp_counter = 0
                     start = 0
                     hypo_counter = 0
@@ -161,29 +125,19 @@ def pp_get(path,name):
                     switch = 0
 
                 elif int(pp_counter) >= 5:
-                    skew = get_AT(name,contig,start,end)                    
-                        
+                    skew = get_GC(name,contig,start,end)                    
                     b.write(
-                    name + '\t' + str(count) + '\t' +str(contig) + '\t' + str(pp_len) + '\t'
-                    + str(hypo_perc)+'\t'+ str(skew) + '\t')
-                    
+                    name+'*' + '\t' + str(count) + '\t' + str(contig) + '\t' + str(start) + '\t' + str(end) + '\t'
+                    +  str(hypo_perc) + 
+                    '\t')
                     if int_id != 0:                                     # If integrase has been found
                         b.write(str(int_id) + '\t' + str(integrase_position) + '\t')
                     else:
                         b.write('\t\t')
-
-                    if repeat_l !=0:                                    # If successful in finding repeat region
-                        b.write(str(repeat_l) + '\t' + str(repeat_r) + '\t' + str(repeat_reason))
-                    else:
-                        b.write('\t\t')
-
                     b.write('\n')
                 
                 int_id = 0
                 int_pos = 0
-                repeat_l = 0
-                repeat_r = 0
-                repeat_reason = 0
                 pp_counter = 0
                 start = 0
                 hypo_counter = 0
